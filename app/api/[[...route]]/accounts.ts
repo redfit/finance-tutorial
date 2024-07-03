3d1b446e015597248f079db1c470fd5818aa2452
redfit <nagita.0724.0812@gmail.com>
2024-07-04 08:25:51 +0900

    accounts bulk delete

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -5,8 +5,9 @@ import { createId } from '@paralleldrive/cuid2'
 import { db } from '@/db/drizzle'
 import { accounts, insertAccountSchema } from '@/db/schema'
 
-import { eq } from 'drizzle-orm'
+import { and, eq, inArray } from 'drizzle-orm'
 import { Hono } from 'hono'
+import { z } from 'zod'
 
 const app = new Hono()
   .get('/', clerkMiddleware(), async (c) => {
@@ -49,6 +50,37 @@ const app = new Hono()
           ...values,
         })
         .returning()
+
+      return c.json({ data })
+    },
+  )
+  .post(
+    '/bulk-delete',
+    clerkMiddleware(),
+    zValidator(
+      'json',
+      z.object({
+        ids: z.array(z.string()),
+      }),
+    ),
+    async (c) => {
+      const auth = getAuth(c)
+      const values = c.req.valid('json')
+      if (!auth?.userId) {
+        return c.json({ error: 'Unauthorized' }, 401)
+      }
+
+      const data = await db
+        .delete(accounts)
+        .where(
+          and(
+            eq(accounts.userId, auth.userId),
+            inArray(accounts.id, values.ids),
+          ),
+        )
+        .returning({
+          id: accounts.id,
+        })
 
       return c.json({ data })
     },
--- components/data-table.tsx
+++ components/data-table.tsx
@@ -17,6 +17,8 @@ import {
 
 import { Trash } from 'lucide-react'
 
+import { useConfirm } from '@/hooks/use-confirm'
+
 import { Input } from '@/components/ui/input'
 import {
   Table,
@@ -44,6 +46,10 @@ export function DataTable<TData, TValue>({
   onDelete,
   disabled,
 }: DataTableProps<TData, TValue>) {
+  const [ConfirmDialog, confirm] = useConfirm({
+    title: 'Are you sure?',
+    message: 'You are about to perform a bulk delete.',
+  })
   const [sorting, setSorting] = React.useState<SortingState>([])
   const [columnFilters, setColumnFilter] = React.useState<ColumnFiltersState>(
     [],
@@ -68,6 +74,7 @@ export function DataTable<TData, TValue>({
 
   return (
     <div>
+      <ConfirmDialog />
       <div className="flex items-center py-4">
         <Input
           placeholder={`Filter ${filterKey}...`}
@@ -83,6 +90,14 @@ export function DataTable<TData, TValue>({
             size="sm"
             variant="outline"
             className="ml-auto font-normal text-xs"
+            onClick={async () => {
+              const ok = await confirm()
+
+              if (ok) {
+                onDelete(table.getFilteredSelectedRowModel().rows)
+                table.resetRowSelection()
+              }
+            }}
           >
             <Trash className="size-4 mr-2" />
             Delete ({table.getFilteredSelectedRowModel().rows.length})
