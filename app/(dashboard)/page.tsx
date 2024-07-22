93b6cf9273ce81750a0db5c8aa91b8f93ec6e77a
redfit <nagita.0724.0812@gmail.com>
2024-07-22 17:04:03 +0900

    Header Component

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -5,5 +5,5 @@ import { UserButton } from "@clerk/nextjs"
 import { Button } from "@/components/ui/button"
 
 export default function Home() {
-  return <UserButton signInUrl="/sign-in" />
+  return <div>Dashboard Page</div>
 }
