c0b8e39bfbb22944d0d31303901d7fd7d3450e1b
redfit <nagita.0724.0812@gmail.com>
2024-07-22 15:24:48 +0900

    hono setup

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image"
 
+import { UserButton } from "@clerk/nextjs"
+
 import { Button } from "@/components/ui/button"
 
 export default function Home() {
-  return <Button>ClickMe!</Button>
+  return <UserButton signInUrl="/sign-in" />
 }
