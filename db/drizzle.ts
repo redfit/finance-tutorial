49caff1d8a437c7f520de42a6d51ab74683ca8cd
redfit <nagita.0724.0812@gmail.com>
2024-07-22 17:50:02 +0900

    drizzle

---
+++ db/drizzle.ts
@@ -0,0 +1,8 @@
+import * as schema from "./schema"
+
+import { drizzle } from "drizzle-orm/neon-http"
+
+import { neon } from "@neondatabase/serverless"
+
+export const sql = neon(process.env.DATABASE_URL!)
+export const db = drizzle(sql, { schema })
---
+++ scripts/migrate.ts
@@ -0,0 +1,21 @@
+import { config } from "dotenv"
+import { drizzle } from "drizzle-orm/neon-http"
+import { migrate } from "drizzle-orm/neon-http/migrator"
+
+import { neon } from "@neondatabase/serverless"
+
+config({ path: ".env.local" })
+
+const sql = neon(process.env.DATABASE_URL!)
+const db = drizzle(sql)
+
+const main = async () => {
+  try {
+    await migrate(db, { migrationsFolder: "drizzle" })
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
+
+main()
