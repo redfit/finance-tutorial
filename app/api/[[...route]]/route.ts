fdd93532325e2c82721f007d7d7935556461055e
redfit <nagita.0724.0812@gmail.com>
2024-07-25 18:01:40 +0900

    Transactions api

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -3,12 +3,16 @@ import { handle } from "hono/vercel"
 
 import accounts from "@/app/api/[[...route]]/accounts"
 import categories from "@/app/api/[[...route]]/categories"
+import transactions from "@/app/api/[[...route]]/transactions"
 
 export const runtime = "edge"
 
 const app = new Hono().basePath("/api")
 
-const routes = app.route("/accounts", accounts).route("/categories", categories)
+const routes = app
+  .route("/accounts", accounts)
+  .route("/categories", categories)
+  .route("/transactions", transactions)
 
 export const GET = handle(app)
 export const POST = handle(app)
